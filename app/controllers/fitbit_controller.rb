f6534c7351285ac16e094713317fccef6a4c8ba0
Bill Peregoy <bill@billperegoy.com>
2014-01-11 19:36:26 -0500

    Chnaged some controller and view names.

--- app/controllers/fitbit_controller.rb
+++ app/controllers/fitbit_controller.rb
@@ -8,7 +8,7 @@ class FitbitController < ApplicationController
     consumer_key = '45eb861ac9084fd6b15305cd298d8189'
     consumer_secret = 'caa8958e83ac48ccac3b0de98458f55b'
 
-    callback_url = 'http://localhost:3000/fitbit/view'
+    callback_url = 'http://localhost:3000/fitbit/get_api_data'
     
     consumer = OAuth::Consumer.new( 
       consumer_key,
@@ -21,19 +21,25 @@ class FitbitController < ApplicationController
     redirect_to request_token.authorize_url
   end
 
-  def view
+  def get_api_data
     request_token = session[:request_token]
-    access_token = request_token.get_access_token(:oauth_verifier => params['oauth_verifier'])
+    @access_token = request_token.get_access_token(:oauth_verifier => params['oauth_verifier'])
 
-    ret_val = access_token.get('/1/user/-/profile.json')
+    user_info
+    weight
+  end
+
+  def user_info
+    ret_val = @access_token.get('/1/user/-/profile.json')
     if ret_val.is_a?(Net::HTTPSuccess)
       @user = JSON.parse(ret_val.body)['user']
     end
+  end
 
-    ret_val = access_token.get('/1/user/-/body/log/weight/date/2014-01-11.json')
+  def weight
+    ret_val = @access_token.get('/1/user/-/body/log/weight/date/2014-01-11/1m.json')
     if ret_val.is_a?(Net::HTTPSuccess)
       @weight = JSON.parse(ret_val.body)['weight']
     end
-
   end
 end
